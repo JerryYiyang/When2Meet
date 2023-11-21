e8799a2182642325dd13dd0ec5c4ccb30465f8de
Michael Huang <94334792+MHFLCMAY2UCSD@users.noreply.github.com>
2023-11-20 20:44:37 -0800

    Inital push

---
+++ when2meet.sql
@@ -0,0 +1,21 @@
+USE when2meet;
+
+CREATE TABLE event_table(
+	eid SERIAL UNIQUE,
+    title VARCHAR(128)
+);
+
+CREATE TABLE availability(
+	session_id SERIAL UNIQUE,
+	user_name VARCHAR(128),
+    date VARCHAR(128),
+    time VARCHAR(128),
+    eid INT REFERENCES event_table(eid)
+);
+
+SELECT * FROM event_table;
+SELECT * FROM availability;
+
+# Get all the names
+SELECT title, user_name, date, time
+FROM event_table INNER JOIN availability;
