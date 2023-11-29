2b4297febe810412e1903f6c43f6d5e446a97900
NotMichaelHuang <eltanin2@yahoo.com>
2023-11-29 12:19:32 -0800

    initial push'

--- when2meet.sql
+++ when2meet.sql
@@ -1,21 +1,24 @@
 USE when2meet;
 
+CREATE TABLE e_date(
+	date_id SERIAL PRIMARY KEY,
+    date_time VARCHAR(128)
+);
+
 CREATE TABLE event_table(
-	eid VARCHAR(128) UNIQUE,
-    title VARCHAR(128)
+	eid VARCHAR(128) PRIMARY KEY,
+    e_name VARCHAR(128)
+);
+
+CREATE TABLE person(
+	pid SERIAL PRIMARY KEY,
+    username VARCHAR(128)
 );
 
 CREATE TABLE availability(
-	session_id SERIAL UNIQUE,
-	user_name VARCHAR(128),
-    date VARCHAR(128),
-    time VARCHAR(128),
-    eid INT REFERENCES event_table(eid)
+	start_time VARCHAR(128),
+    end_time VARCHAR(128),
+    p_name VARCHAR(128) REFERENCES person(username),
+    event_date VARCHAR(128) REFERENCES e_date(date_time),
+    event_id VARCHAR(128) REFERENCES event_table(eid)
 );
-
-SELECT * FROM event_table;
-SELECT * FROM availability;
-
-# Get all the names
-SELECT title, user_name, date, time
-FROM event_table INNER JOIN availability;
