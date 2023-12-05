3b59daaaeec4282b452d83d74dff312b6d2631b0
David Cho <davidcho@Davids-Air-37.lan>
2023-12-04 23:25:49 -0800

    join pages complete

--- src/main/java/com/example/groupproject/EndJoinPageController.java
+++ src/main/java/com/example/groupproject/EndJoinPageController.java
@@ -1,4 +1,24 @@
 package com.example.groupproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import java.io.IOException;
+
 public class EndJoinPageController {
+    @FXML
+    private Label confirmationLabel;
+    @FXML
+    private Button returnButton;
+
+    public void initialize() {
+        // Display the confirmation message
+        confirmationLabel.setText("You have successfully joined the event!");
+    }
+
+    @FXML
+    private void onReturnButtonClick() throws IOException {
+        // Code to return to the main page or relevant section
+        Main.loadStartPage();
+    }
 }
