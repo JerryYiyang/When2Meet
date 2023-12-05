59578617acc3bbf021ac257f98a4a1c38dafccfd
JerryYiyang <66449672+JerryYiyang@users.noreply.github.com>
2023-12-05 15:21:27 -0800

    added multiple database connection methods

--- src/main/java/com/example/groupproject/DatabaseConnection.java
+++ src/main/java/com/example/groupproject/DatabaseConnection.java
@@ -37,6 +37,91 @@ public class DatabaseConnection {
         }
     }
 
+    public void enterDates(ArrayList<String> dates){
+        try {
+            PreparedStatement ps = connect.prepareStatement("INSERT INTO dates VALUES (?)");
+
+            for (String date : dates) {
+                ps.setString(1, date);
+                ps.executeUpdate();
+            }
+            connect.commit();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addPerson(String name){
+        try{
+            PreparedStatement ps = connect.prepareStatement("INSERT INTO person VALUES (?)");
+            ps.setString(1, name);
+            ps.executeUpdate();
+            connect.commit();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void enterAvailability(String start_time, String end_time, String person, String date,
+                                  String event){
+        try{
+            PreparedStatement ps = connect.prepareStatement("INSERT INTO availability" +
+                    "VALUES (?, ?, ?, ?, ?)");
+            ps.setString(1, start_time);
+            ps.setString(2, end_time);
+            ps.setString(3, person);
+            ps.setString(4, date);
+            ps.setString(5, event);
+            ps.executeUpdate();
+            connect.commit();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateAvailability(String start_time, String end_time, String person, int date,
+                                   String event){
+        try{
+            PreparedStatement ps = connect.prepareStatement(
+                    "UPDATE availability SET start_time = ?, end_time = ? WHERE p_name = ? AND eid = ? AND date_id = ?");
+            ps.setString(1, start_time);
+            ps.setString(2, end_time);
+            ps.setString(3, person);
+            ps.setString(4, event);
+            ps.setInt(5, date);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String[]> getAvailability(String event_id, String date_id){
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<String[]> result = new ArrayList<>();
+        try{
+            ps = connect.prepareStatement("SELECT a.start_time, a.end_time" +
+                    "FROM availability a join dates d on a.date_id = d.date_id" +
+                    "WHERE a.eid = ? AND d.date_id = ?");
+            ps.setString(1, event_id);
+            ps.setString(2, date_id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String startTime = rs.getString("start_time");
+                String endTime = rs.getString("end_time");
+                result.add(new String[]{startTime, endTime});
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeResources(rs, ps);
+        return result;
+    }
+
     public Boolean checkID(String id){
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -54,21 +139,6 @@ public class DatabaseConnection {
         }
 
         return isEidPresent;
-    }
-
-    public void enterDates(ArrayList<String> dates){
-        try {
-            PreparedStatement ps = connect.prepareStatement("INSERT INTO dates VALUES (?)");
-
-            for (String date : dates) {
-                ps.setString(1, date);
-                ps.executeUpdate();
-            }
-            connect.commit();
-            ps.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void closeConnection() {
